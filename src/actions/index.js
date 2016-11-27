2c827653d6ec547b0529feacd2fbfe08137a8487
MC3D <m.chapman.home@gmail.com>
2016-11-27 17:38:56 -0500

    sets up reducer_active_book

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,8 @@
 export function selectBook(book) {
-  console.log(book.title);
+  // selectbook is an ActionCreator, it needs to return an ation,
+  // an object with a type property
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
