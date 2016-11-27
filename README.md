5802ce0e049291a1642a4da34d7feef4aade9398
MC3D <m.chapman.home@gmail.com>
2016-11-27 17:25:43 -0500

    connects selectBook action and book-list container

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Notes
 
 reducer: a function that returns a piece of application state
+
+container: normal react component that gets connected to application state
--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList () {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className='list-group-item'>{book.title}</li>
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className='list-group-item'>
+          {book.title}
+        </li>
       );
     });
   }
@@ -26,4 +33,12 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+// whatever gets returned will end up as props on BookList container
+function mapDispatchToProps(dispatch) {
+  // whenever selectBook is called, result is passed to all reducers
+  return bindActionCreators( { selectBook: selectBook }, dispatch)
+}
+
+// promote BookList from component to container - it needs to know about
+// this new dispatch method, selectBook; make it available as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
